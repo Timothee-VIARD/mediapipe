a11bc3a8f3858b75aeaee38771d8b128ac7e17f9
Timothée <timothee.viardtv@gmail.com>
2024-09-19 12:10:47 +0100

    Add a pitch shifter demo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const mediapipeButton = document.querySelector("#mediapipeButton");
 const webRTCButton = document.querySelector("#webRTCButton");
+const pitchShiftButton = document.querySelector("#pitchShiftButton");
 
 mediapipeButton.addEventListener("click", () => {
   location.href = "/src/Mediapipe.html";
@@ -7,4 +8,8 @@ mediapipeButton.addEventListener("click", () => {
 
 webRTCButton.addEventListener("click", () => {
   location.href = "/src/webRTC/webRTC.html";
+});
+
+pitchShiftButton.addEventListener("click", () => {
+  location.href = "/src/pitchShift/pitchShift.html";
 });
